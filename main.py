cb955027ccb85fb597930404f801f62f5faf663b
maybegal <galshichrur@gmail.com>
2024-10-20 01:37:07 +0300

    rebuild main.py

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from fastapi import FastAPI
-from request import get_response
-from models import Challenge, Feedback
+from fastapi import FastAPI, HTTPException
+import request
+from models import Challenge
 
 app = FastAPI()
 
 
-@app.get("/", response_model=Feedback)
-def get_feedback(challenge: Challenge) -> Feedback:
-    feedback_data = get_response(challenge)
-
-    feedback = Feedback(
-        grade=feedback_data["grade"],
-        overview=feedback_data["overview"],
-        strategy=feedback_data["strategy"],
-        solution=feedback_data["solution"],
-        code_solution=feedback_data["code_solution"],
-        growth=feedback_data["growth"]
-    )
-
-    return feedback
+@app.post("/feedback/grade", response_model=int)
+def get_grade(challenge: Challenge) -> int:
+    pass
 
 
 if __name__ == '__main__':
