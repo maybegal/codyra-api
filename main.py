5c07bd4224b284e22d684b1a88abcdf3df6f1292
maybegal <galshichrur@gmail.com>
2024-10-19 19:21:58 +0300

    update main.py to be simple and not use complicated arrays

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from uuid import UUID, uuid4
+#  from uuid import UUID, uuid4
 from pydantic import BaseModel
 from typing import Optional
 app = FastAPI()
 
 
 class Task(BaseModel):
-    id: Optional[UUID] = None
     programming_language: str
     question: str
     answer: str
     notes: Optional[str] = ""
-    completed: bool = False
-
-
-tasks: list[Task] = []
-
-
-@app.post("/tasks/", response_model=Task)
-def create_task(task: Task):
-    task.id = uuid4()
-    tasks.append(task)
-    return task
-
-
-@app.get("/tasks/", response_model=list[Task])
-def read_tasks():
-    return tasks
-
-
-@app.get("/tasks/{task_id}", response_model=Task)
-def read_task(task_id: UUID):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-
-    return HTTPException(status_code=404, detail="Task not found")
 
 
 if __name__ == '__main__':
