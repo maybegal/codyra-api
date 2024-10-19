29507edfb6a74171aedb40f4ddecb8954a98bf73
maybegal <galshichrur@gmail.com>
2024-10-19 17:33:39 +0300

    add question object and class

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class Question:
+    programming_language: str
+    question: str
+    answer: str
+    notes: str = ""
+    completed = False
+
+    def __init__(self, programming_language: str, question: str, answer: str, notes: str = ""):
+        self.programming_language = programming_language
+        self.question = question
+        self.answer = answer
+        self.notes = notes
+        self.completed = False
+
+@app.post("/questions/", response_model=Question)
+def create_question(question: Question):
+    pass
+
 @app.get("/")
 def read():
     return {"hello": "world"}
